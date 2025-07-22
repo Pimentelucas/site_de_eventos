95993432b6b71bf8818556c3ef6ded3d0b287232
pimentelucas <pimentellucas753@gmail.com>
2025-07-22 15:00:30 -0300

    feat()cria formulario

--- routes/web.php
+++ routes/web.php
@@ -7,20 +7,10 @@ Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 Route::get('/events/login', [EventController::class, 'login']);
 Route::get('/events/cadastro', [EventController::class, 'cadastro']);
+Route::post('/events', [EventController::class, 'store']);
 
 
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/produtos', function () {
-
-    $busca = request('search');
-
-    return view('products', ['busca' => $busca]);
-});
-
-Route::get('/produtos_teste/{id}', function ($id = null) {
-    return view('product', ['id' => $id]);
 });
